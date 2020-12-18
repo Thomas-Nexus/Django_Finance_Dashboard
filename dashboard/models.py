a52251236963376302d0fad9ff30fa21b1943e4e
Thomas-Nexus <72507931+Thomas-Nexus@users.noreply.github.com>
2020-12-18 15:00:48 +0000

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-class Club(models.Model):
-    name = models.CharField(max_length=220)
-    money = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.name}, {self.money}'
-
-
